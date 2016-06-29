37f307a77009925641b599031bb43a5f29f86fcd
Stephan Zuercher <zuercher@gmail.com>
2016-06-29 23:36:09 +0000

    logparser: compose forwarder/parser FromFlags and move implementation out of main (#750)
    fbshipit-source-id: 75fcc77

--- tempfile/tempfile.go
+++ tempfile/tempfile.go
@@ -10,6 +10,8 @@ import (
 	"testing"
 )
 
+const defaultPermissions = 0666
+
 // Generates an empty temporary file and returns its name and a
 // cleanup function that removes the file. The prefix is optional and
 // may be used to control the name of the temporary file. Typically,
@@ -39,10 +41,23 @@ func Make(t *testing.T, prefix ...string) (string, func()) {
 // to create the file.
 func Write(t *testing.T, data string, prefix ...string) (string, func()) {
 	filename, cleanup := Make(t, prefix...)
-	err := ioutil.WriteFile(filename, []byte(data), 0666)
+	err := ioutil.WriteFile(filename, []byte(data), defaultPermissions)
 	if err != nil {
 		t.Fatalf("failed to write temp file for test: %v", err)
 		return "", func() {}
 	}
 	return filename, cleanup
+}
+
+// Appends the given data to a previously created file.
+func Append(t *testing.T, file, data string) error {
+	f, err := os.OpenFile(file, os.O_WRONLY|os.O_APPEND, defaultPermissions)
+	if err != nil {
+		t.Fatalf("failed to append to temp file '%s' for test: %v", file, err)
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(data)
+	return err
 }
