c0f694ffe09bcc5300d6d32c00b4ce0bc908e1dd
Stephan Zuercher <zuercher@gmail.com>
2016-11-04 22:21:39 +0000

    executor: implement scatter/gather and timeouts (fixes #1309) (#1684)
    * executor: basic scatter/gather
    * executor: introduce timeouts
    * executor: fix bugs related to timeout & race conditions; flesh out some testing
    * logparser: configure executor logger
    * logparser, stats: use Context to allow timeouts
    
    fbshipit-source-id: 27a0066

--- log/log.go
+++ log/log.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"bytes"
 	"log"
 
 	"github.com/turbinelabs/test/io"
@@ -8,4 +9,9 @@ import (
 
 func NewNoopLogger() *log.Logger {
 	return log.New(io.NewNoopWriter(), "", log.LstdFlags)
+}
+
+func NewBufferLogger() (*log.Logger, *bytes.Buffer) {
+	buf := bytes.NewBuffer(make([]byte, 0, 1024))
+	return log.New(buf, "", log.LstdFlags), buf
 }
