2ae40f8e46ceca1e9ab36786505c5b76a4c320ab
Stephan Zuercher <stephan@turbinelabs.io>
2017-06-09 22:25:22 +0000

    test/matcher: introduce CaptureAll, CaptureAllMatching; clean up godoc
    fbshipit-source-id: b2f31d2

--- matcher/captor.go
+++ matcher/captor.go
@@ -21,21 +21,32 @@ import (
 	"reflect"
 )
 
-// CaptureAny will match and capture any value.
+// CaptureAny will match any value and captures the most recent one.
 func CaptureAny() *ValueCaptor {
-	return &ValueCaptor{nil, nil}
+	return &ValueCaptor{}
 }
 
-// CaptureMatching is a ValueCaptor that proxies the given Matcher to match
-// and capture matching values.
+// CaptureAll will match and capture multiple values of any type.
+func CaptureAll() *AllValueCaptor {
+	return &AllValueCaptor{}
+}
+
+// CaptureMatching is a ValueCaptor that proxies the given Matcher to
+// match and capture the most recent matching value.
 func CaptureMatching(m Matcher) *ValueCaptor {
-	return &ValueCaptor{m, nil}
+	return &ValueCaptor{mustMatch: m}
 }
 
-// CaptureType is a ValueCaptor that uses an IsOfType matcher to capture
-// values of the given reflect.Type.
+// CaptureAllMatching is an AllValueCaptor that proxies the given
+// Matcher to match and capture all matching values.
+func CaptureAllMatching(m Matcher) *AllValueCaptor {
+	return &AllValueCaptor{mustMatch: m}
+}
+
+// CaptureType is a ValueCaptor that uses an IsOfType matcher to
+// capture the most recent value of the given reflect.Type.
 func CaptureType(t reflect.Type) *ValueCaptor {
-	return &ValueCaptor{IsOfType{t}, nil}
+	return &ValueCaptor{mustMatch: IsOfType{t}}
 }
 
 // A ValueCaptor can be wrapped around a Matcher to produce a Matcher
@@ -47,6 +58,10 @@ type ValueCaptor struct {
 
 var _ Matcher = &ValueCaptor{}
 
+// Matches compares x to the underlying Matcher, if any. If x matches
+// (or there is no underlying Matcher), it returns true and captures
+// x. If a previous value was captured, it is forgotten. If x does not
+// match, it returns false.
 func (vc *ValueCaptor) Matches(x interface{}) bool {
 	if vc.mustMatch != nil && !vc.mustMatch.Matches(x) {
 		return false
@@ -57,5 +72,34 @@ func (vc *ValueCaptor) Matches(x interface{}) bool {
 }
 
 func (vc *ValueCaptor) String() string {
-	return fmt.Sprintf("valueCaptor(mustMatch: %s)", vc.mustMatch)
+	return fmt.Sprintf("ValueCaptor(mustMatch: %v)", vc.mustMatch)
+}
+
+// An AllValueCaptor can be wrapped around a Matcher to produce a
+// Matcher which also captures all matched values.
+type AllValueCaptor struct {
+	mustMatch Matcher
+	V         []interface{}
+}
+
+// Matches compares x to the underlying Matcher, if any. If x matches
+// (or there is no underlying Matcher), it returns true and captures
+// x. If x does not match, it returns false.
+func (avc *AllValueCaptor) Matches(x interface{}) bool {
+	if avc.mustMatch != nil && !avc.mustMatch.Matches(x) {
+		return false
+	}
+
+	if avc.V == nil {
+		avc.V = make([]interface{}, 1)
+		avc.V[0] = x
+	} else {
+		avc.V = append(avc.V, x)
+	}
+
+	return true
+}
+
+func (avc *AllValueCaptor) String() string {
+	return fmt.Sprintf("AllValueCaptor(mustMatch: %v)", avc.mustMatch)
 }
--- matcher/captor_test.go
+++ matcher/captor_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package matcher
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
@@ -30,12 +31,29 @@ type teststruct struct {
 
 func TestCaptureAny(t *testing.T) {
 	cap := CaptureAny()
-	passed := 1234
+	assert.Equal(t, cap.String(), "ValueCaptor(mustMatch: <nil>)")
 	assert.True(t, cap.Matches(1234))
 
 	asInt, ok := cap.V.(int)
 	assert.True(t, ok)
-	assert.Equal(t, asInt, passed)
+	assert.Equal(t, asInt, 1234)
+}
+
+func TestCaptureAll(t *testing.T) {
+	cap := CaptureAll()
+	assert.Equal(t, cap.String(), "AllValueCaptor(mustMatch: <nil>)")
+	assert.True(t, cap.Matches(1234))
+	assert.True(t, cap.Matches(5678))
+
+	assert.Equal(t, len(cap.V), 2)
+
+	asInt, ok := cap.V[0].(int)
+	assert.True(t, ok)
+	assert.Equal(t, asInt, 1234)
+
+	asInt, ok = cap.V[1].(int)
+	assert.True(t, ok)
+	assert.Equal(t, asInt, 5678)
 }
 
 type maybeMatcher struct {
@@ -47,13 +65,14 @@ func (mm maybeMatcher) Matches(_ interface{}) bool {
 }
 
 func (mm maybeMatcher) String() string {
-	return ""
+	return fmt.Sprintf("maybe=%t", mm.shouldMatch)
 }
 
 func TestCaptureMatching(t *testing.T) {
 	want := teststruct{1234, "aoeu"}
 	cap := CaptureMatching(maybeMatcher{true})
 
+	assert.Equal(t, cap.String(), "ValueCaptor(mustMatch: maybe=true)")
 	assert.True(t, cap.Matches(want))
 
 	asTs, ok := cap.V.(teststruct)
@@ -66,6 +85,47 @@ func TestCaptureMatchingDidNotMatch(t *testing.T) {
 	cap := CaptureMatching(maybeMatcher{false})
 
 	assert.False(t, cap.Matches(doNotWant))
+	assert.Nil(t, cap.V)
+}
+
+type oddMatcher struct{}
+
+func (om oddMatcher) Matches(x interface{}) bool {
+	if i, ok := x.(int); ok {
+		return i&1 == 1
+	}
+
+	return false
+}
+
+func (om oddMatcher) String() string {
+	return "odd"
+}
+
+func TestCaptureAllMatching(t *testing.T) {
+	cap := CaptureAllMatching(oddMatcher{})
+
+	assert.Equal(t, cap.String(), "AllValueCaptor(mustMatch: odd)")
+	assert.True(t, cap.Matches(123))
+	assert.False(t, cap.Matches(456))
+	assert.True(t, cap.Matches(789))
+
+	assert.Equal(t, len(cap.V), 2)
+
+	asInt, ok := cap.V[0].(int)
+	assert.True(t, ok)
+	assert.Equal(t, asInt, 123)
+
+	asInt, ok = cap.V[1].(int)
+	assert.True(t, ok)
+	assert.Equal(t, asInt, 789)
+}
+
+func TestCaptureAllMatchingDidNotMatch(t *testing.T) {
+	cap := CaptureMatching(oddMatcher{})
+
+	assert.False(t, cap.Matches(2))
+	assert.False(t, cap.Matches(4))
 	assert.Nil(t, cap.V)
 }
 
